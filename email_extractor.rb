2f59cefc218a8873d6ca548a7106268519128caa
vickyzheng94 <vickyzheng94@gmail.com>
2015-12-20 01:29:26 -0500

    A program that is able to scrap all the emails from hicomb's papers

---
+++ email_extractor.rb
@@ -0,0 +1,71 @@
+require 'pdf-reader'
+require 'open-uri'
+
+puts "Hello, this program extracts emails from papers found in http://www.hicomb.org/"
+
+year = 2 #because it starts at 2003 
+article_no = 0
+#Use begin, rescue, else 
+year_string = " "
+article_no_string = " "
+
+last_year = 15
+all_emails = [];
+while(year < last_year)
+	if(year < 10) 
+		year_string = "0#{year}"
+	else 
+		year_string = "#{year}"
+	end
+
+	if(article_no < 10) 
+		article_no_string = "0#{article_no}"
+	else 
+		article_no_string = "#{article_no}"
+	end
+	link = "http://www.hicomb.org/HiCOMB2015/papers/HICOMB20" + year_string + "-" + article_no_string + ".pdf"
+	begin
+		io = open(link)
+	rescue OpenURI::HTTPError => error
+		response = error.io
+		puts response.status
+		puts response.string
+		year = year + 1 
+		article_no = 0
+	end
+	reader = PDF::Reader.new(io)
+	num_pages = reader.pages.size
+	page_index = 0
+	while page_index < num_pages
+		begin
+			page_text = reader.pages[page_index].text
+		rescue
+			puts "I am trying to rescue"
+			page_index = page_index + 1
+			retry while (page_index < num_pages)
+		end
+		puts "Page text class: #{page_text.class}" 
+		puts "Page index: #{page_index}" 
+		emails = page_text.scan(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i)
+		if emails.any? == true
+			all_emails = all_emails + emails;
+		end
+		page_index = page_index + 1
+	end
+	puts "year: #{year}" 
+	puts "Article number: #{article_no}" 
+	article_no = article_no + 1
+end
+
+num_emails = all_emails.size
+email = 0
+while email_num < num_emails
+	email = all_emails[email_num]
+	email = email + "\n"
+	open('emails.txt', 'a') do |f|
+		f << email
+	end
+	email = email + 1
+end
+
+
